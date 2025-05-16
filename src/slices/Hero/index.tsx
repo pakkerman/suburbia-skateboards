f597d5ad9337ba7f43d77ac74552cfdda2a45329
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-16 16:37:59 +0800

    feat: add hero bg logo

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,11 +4,13 @@ import {
   PrismicText,
   SliceComponentProps,
 } from "@prismicio/react";
-import { PrismicNextLink } from "@prismicio/next";
 import { Content } from "@prismicio/client";
 
 import { Bounded } from "@/app/components/Bounded";
 import { Heading } from "@/app/components/Heading";
+import { ButtonLink } from "@/app/components/ButtonLink";
+import { WideLogo } from "./WideLogo";
+import { TallLogo } from "./TallLogo";
 
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
@@ -17,18 +19,29 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="bg-texture relative h-dvh bg-brand-pink text-zinc-800"
+      className="bg-texture relative h-dvh overflow-hidden bg-brand-pink text-zinc-800"
     >
+      <div className="absolute inset-0 flex items-center pt-20 ">
+        <WideLogo className="hidden w-full text-brand-purple opacity-20 mix-blend-multiply lg:block" />
+        <TallLogo className="w-full text-brand-purple opacity-20 mix-blend-multiply lg:hidden" />
+      </div>
       <div className="absolute inset-0 mx-auto mt-24 grid max-w-6xl grid-rows-[1fr,auto] place-items-end px-6 ~py-10/16 ">
         <Heading size="lg" className="relative max-w-2xl place-self-start">
           <PrismicText field={slice.primary.heading} />
         </Heading>
 
-        <div className="flex justify-between flex-col items-center w-full lg:flex-row ~gap-2/4">
+        <div className="flex w-full flex-col items-center justify-between ~gap-2/4 lg:flex-row">
           <div className="max-w-[45ch] font-semibold ~text-lg/xl">
             <PrismicRichText field={slice.primary.body} />
           </div>
-          <PrismicNextLink field={slice.primary.button} />
+          <ButtonLink
+            field={slice.primary.button}
+            size="lg"
+            className="z-20 mt-2 block"
+            icon="skateboard"
+          >
+            {slice.primary.button.text}
+          </ButtonLink>
         </div>
       </div>
 
