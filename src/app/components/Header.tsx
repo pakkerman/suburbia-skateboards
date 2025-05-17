224a244fd3a2977729bfc5852d8755857b9cc559
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-17 13:40:33 +0800

    feat: add nav link from settings

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 import { ButtonLink } from "@/app/components/ButtonLink";
 import { Logo } from "@/app/components/Logo";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
 
-type Props = {};
+export default async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
 
-export default function Header({}: Props) {
   return (
     <header className="header absolute left-0 right-0 top-0 z-50 ~h-32/48 ~px-4/6 ~py-4/6 md:h-32">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
@@ -17,8 +20,11 @@ export default function Header({}: Props) {
           className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1"
         >
           <ul className="flex flex-wrap items-center justify-center gap-8">
-            <li>Boards</li>
-            <li>Boards</li>
+            {settings.data.navigation.map((item) => (
+              <li key={item.link.text}>
+                <PrismicNextLink field={item.link} className="~text-lg/xl" />
+              </li>
+            ))}
           </ul>
         </nav>
         <div className="justify-self-end">
