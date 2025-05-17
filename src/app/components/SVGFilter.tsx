24174b7762e33ba44a7e6d1e2b5a47dc4108588a
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-17 13:26:54 +0800

    feat: apply squiggle

--- src/app/components/SVGFilter.tsx
+++ src/app/components/SVGFilter.tsx
@@ -2,14 +2,14 @@ export function SVGFilters() {
   return (
     <svg className="h-0 w-0">
       <defs>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <filter id={`squiggle-${index}`} key={index}>
+        {Array.from({ length: 5 }).map((_, idx) => (
+          <filter id={`squiggle-${idx}`} key={idx}>
             <feTurbulence
               baseFrequency="0.05"
               id="turbulence"
               numOctaves="2"
               result="noise"
-              seed={index}
+              seed={idx}
             />
             <feDisplacementMap
               id="displacement"
--- src/slices/Hero/WideLogo.tsx
+++ src/slices/Hero/WideLogo.tsx
@@ -1,3 +1,5 @@
+import { clsx } from "clsx";
+
 import { SVGProps } from "react";
 
 export function WideLogo({ className, ...props }: SVGProps<SVGSVGElement>) {
@@ -6,7 +8,7 @@ export function WideLogo({ className, ...props }: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 1440 553"
-      className={className}
+      className={clsx("animate-squiggle", className)}
       {...props}
     >
       <path
