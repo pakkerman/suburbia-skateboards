0529f239e132732e5d39eb558c27ec40094996b5
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-20 17:54:21 +0800

    feat: add all skaters into TeamGrid

--- src/slices/TeamGrid/index.tsx
+++ src/slices/TeamGrid/index.tsx
@@ -1,50 +1,34 @@
-import { FC } from "react";
+import { FC, Fragment, JSX } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicText, SliceComponentProps } from "@prismicio/react";
 
-/**
- * Props for `TeamGrid`.
- */
+import { Bounded } from "@/app/components/Bounded";
+import { Heading } from "@/app/components/Heading";
+import { createClient } from "@/prismicio";
+import Skater from "./Skater";
+
 export type TeamGridProps = SliceComponentProps<Content.TeamGridSlice>;
 
-/**
- * Component for "TeamGrid" Slices.
- */
-const TeamGrid: FC<TeamGridProps> = ({ slice }) => {
+const TeamGrid: FC<TeamGridProps> = async ({ slice }): Promise<JSX.Element> => {
+  const client = createClient();
+  const skaters = await client.getAllByType("skater");
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-texture bg-brand-navy"
     >
-      Placeholder component for team_grid (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
-    </section>
+      <Heading as="h2" size="lg" className="mb-8 text-center text-white">
+        <PrismicText field={slice.primary.heading} />
+      </Heading>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+        {skaters.map((skater, idx) => (
+          <Fragment key={idx}>
+            {skater.data.first_name && <Skater skater={skater} index={idx} />}
+          </Fragment>
+        ))}
+      </div>
+    </Bounded>
   );
 };
 
