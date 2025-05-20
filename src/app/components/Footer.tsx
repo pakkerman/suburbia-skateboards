7ea8ca2bc07f7c4a751ab514319116e8918623b8
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-20 18:43:03 +0800

    feat: footer board physics box

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,51 @@
+import { createClient } from "@/prismicio";
+import { PrismicNextImage } from "@prismicio/next";
+import { asImageSrc } from "@prismicio/client";
+
+import { Logo } from "@/app/components/Logo";
+import { Bounded } from "./Bounded";
+import { PrismicLink } from "@prismicio/react";
+import { FooterPhysics } from "./FooterPhysics";
+
 type Props = {};
 
-export default function Footer({}: Props) {
-  return <footer className=""></footer>;
+export default async function Footer({}: Props) {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  const boardTextureURLs = settings.data.footer_skateboards
+    .map((item) => asImageSrc(item.skateboard, { h: 600 }))
+    .filter((url): url is string => Boolean(url));
+
+  return (
+    <footer className="bg-texture bg-zinc-900 text-white">
+      <div className="relative h-[75vh] ~p-10/16 md:aspect-auto">
+        {/* Images */}
+        <PrismicNextImage
+          field={settings.data.footer_image}
+          alt=""
+          fill
+          className="object-cover "
+          width={1200}
+        />
+        {/* Physics sandbox for Board */}
+        <FooterPhysics
+          boardTextureURLs={boardTextureURLs}
+          className="absolute inset-0 overflow-hidden"
+        />
+        {/* Logo */}
+        <Logo className="pointer-events-none relative h-20 mix-blend-exclusion md:h-28" />
+      </div>
+      <Bounded as="nav" className="">
+        <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
+          {settings.data.navigation.map((item) => (
+            <li key={item.link.text} className="hover:underline">
+              <PrismicLink field={item.link} />
+            </li>
+          ))}
+        </ul>
+        {/* Footer nav */}
+      </Bounded>
+    </footer>
+  );
 }
