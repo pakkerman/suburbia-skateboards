6977b9809cdc8646d9cc9764744b40947d9a34c9
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-27 10:07:47 +0800

    feat: set camera position when select parts

--- src/app/build/preview.tsx
+++ src/app/build/preview.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useRef } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import {
   CameraControls,
   Environment,
@@ -37,6 +37,40 @@ export default function Preview({ wheelTextureURLs, deckTextureURLs }: Props) {
     asImageSrc(selectedDeck?.texture) ?? DEFAULT_DECK_TEXTURE;
   const truckColor = selectedTruck?.color ?? DEFAULT_TRUCK_COLOR;
   const boltColor = selectedBolt?.color ?? DEFAULT_BOLT_COLOR;
+
+  useEffect(() => {
+    setCameraControls(
+      new THREE.Vector3(0, 0.3, 0),
+      new THREE.Vector3(1.5, 0.8, 0),
+    );
+  }, [selectedDeck]);
+
+  useEffect(() => {
+    setCameraControls(
+      new THREE.Vector3(-0.12, 0.29, 0.57),
+      new THREE.Vector3(0.1, 0.25, 0.9),
+    );
+  }, [selectedTruck]);
+
+  useEffect(() => {
+    setCameraControls(
+      new THREE.Vector3(-0.08, 0.54, 0.64),
+      new THREE.Vector3(0.09, 1, 0.9),
+    );
+  }, [selectedWheel]);
+  useEffect(() => {
+    setCameraControls(
+      new THREE.Vector3(-0.25, 0.3, 0.62),
+      new THREE.Vector3(-0.5, 0.35, 0.8),
+    );
+  }, [selectedBolt]);
+
+  function setCameraControls(target: THREE.Vector3, pos: THREE.Vector3) {
+    if (!cameraControls.current) return;
+
+    cameraControls.current.setTarget(target.x, target.y, target.z, true);
+    cameraControls.current.setPosition(pos.x, pos.y, pos.z, true);
+  }
 
   // prevent clip through floor
   function onCameraControlStart() {
